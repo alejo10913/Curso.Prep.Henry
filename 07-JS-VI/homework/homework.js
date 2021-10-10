026f01fd3314654df03daff85a5f907ec7f16369
alejo10913 <joagonzalezfi@unal.edu.co>
2021-10-09 23:10:57 -0500

    homework finalizada

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -40,6 +40,17 @@ function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+
+  // forma 1
+  // for (i = 0; i < array.length; i++){
+  //   cb(array[i]);
+  // }
+
+  // forma 2
+array.forEach(function(elemento){
+  cb(elemento);
+})
+
 }
 
 function map(array, cb) {
@@ -47,12 +58,33 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  // var nuevoarray = []; 
+  // for (i = 0; i < array.length; i++){
+  //   nuevoarray.push(cb(array[i]));
+  // }
+  // return nuevoarray;
+  var nuevoarray = array.map(function(elemento){
+   return cb(elemento);
+  })
+  return nuevoarray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  // var newArray = [];
+  // for (i = 0; i < array.length; i++){
+  //   if (array[i][0] === "a"){
+  //     newArray.push(array[i]);
+  //   }
+
+  // }
+  // return newArray;
+  var nuevoarray = array.filter(function(elemento){
+    return elemento[0] === "a";
+  })
+  return nuevoarray;
 }
 
 // No modificar nada debajo de esta línea
